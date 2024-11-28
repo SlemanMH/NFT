1bde4d2af6f4d8da3643a745214d414897beabc3
SlemanMH <164754786+SlemanMH@users.noreply.github.com>
2024-11-28 18:25:31 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll(".animation");
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        } else {
+          entry.target.classList.remove("visible");
+        }
+      });
+    });
+  
+    sections.forEach((section) => {
+      observer.observe(section);
+    });
+  });
+  
+  
